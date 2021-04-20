bfae0ec7d5dd2dd1178e36f4d5d341976bf92c4d
Joy Kirii <joykirii@gmail.com>
2021-04-20 20:00:10 +0300

    deployment to heroku

--- start.sh
+++ start.sh
@@ -1,7 +1,8 @@
 
-# export NEWS_API_KEY='ba9597fd625949abbc62d870f9db373c'
-# export NEWS_API_KEY='daeb9fd9738b41ae858c5e23ed438a9b' 
-export NEWS_API_KEY='0fb1cd6ef5614bfca5182c188b85f2b9'
+
+export NEWS_API_KEY='6335023d9b70450b9a2924739e948c0c'
+
+
 
 
 
